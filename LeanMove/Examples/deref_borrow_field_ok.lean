55f101452d45437e6a23019547b21d8872b09777
Ilya Sergey <ilya@nus.edu.sg>
2025-12-04 19:15:22 +0800

    rename

--- LeanMove/Examples/deref_borrow_field_ok.lean
+++ LeanMove/Examples/deref_borrow_field_ok.lean
@@ -31,80 +31,36 @@ open AssocMap
 /-
 Original Move IR Program:
 
-module 0x42.M {
-  struct T { f: u64 }
+module 0x1.M {
+    struct T has drop {f: u64}
 
-  new(x: u64): Self.T {
-    return T { f: move(x) };
-  }
+    public new(g: u64): Self.T {
+    label b0:
+        return T{f: move(g)};
+    }
 
-  t(r: &Self.T) {
+    public t(this: &Self.T) {
+        let y: u64;
+    label b0:
+        y = *&move(this).T::f; // valid deref/read
+        return;
+    }
+}
+
+//# run
+module 0x42.m {
+import 0x1.M;
+
+entry foo() {
+    let x: M.T;
+    let x_ref: &M.T;
+    x = M.new(2);
+    x_ref = &x;
+    M.t(move(x_ref));
     return;
-  }
-
-  entry foo() {
-    let x: Self.T;
-    x = Self.new(2);
-    Self.t(&x);
-    return;
-  }
+}
 }
 -/
 
--- Define the struct type T with field f: u64
-def typeT : BasicMoveType :=
-  .trecord (insert AssocMap.empty ⟨"f"⟩ .tint)
-
--- Function foo() translated to MoveLight A-Normal Form
-def exampleFoo : Stmt :=
-  .seq
-    -- let s1 = copy(literal_2)
-    (.letBind (Site.site 1) (Expr.usage (Usage.copy ⟨"literal_2"⟩)))
-    (.seq
-      -- let s2 = M.new(s1)  [consumes s1, produces s2: T]
-      (.call [Site.site 2] "M.new" [Site.site 1])
-      (.seq
-        -- x = s2  [moves s2 into variable x]
-        (.assign ⟨"x"⟩ (Site.site 2))
-        (.seq
-          -- let s3 = &x  [creates immutable borrow of x]
-          (.letBind (Site.site 3) (Expr.usage (Usage.borrowImm ⟨"x"⟩)))
-          (.seq
-            -- M.t(s3)  [consumes the borrow s3]
-            (.call [] "M.t" [Site.site 3])
-            -- return ()
-            (.return [])))))
-
--- Function M.new(x: u64): T
-def funDefNew : FunDef :=
-  { params := [(⟨"x"⟩, .basic .tint)]
-  , returnType := .basic typeT
-  , locals := []
-  , body :=
-      .seq
-        -- let s1 = copy(literal_2) [for field value]
-        (.letBind (Site.site 1) (Expr.usage (Usage.copy ⟨"x"⟩)))
-        (.seq
-          -- let s2 = T { f: s1 }
-          (.letBind (Site.site 2) (Expr.pack "T" [(⟨"f"⟩, Site.site 1)]))
-          -- return s2
-          (.return [Site.site 2]))
-  }
-
--- Function M.t(r: &T): ()
-def funDefT : FunDef :=
-  { params := [(⟨"r"⟩, .ref typeT (.varRef ⟨"r"⟩) .siteBorrowImm)]
-  , returnType := .basic .tunit
-  , locals := []
-  , body := .return []
-  }
-
--- Entry function foo(): ()
-def funDefFoo : FunDef :=
-  { params := []
-  , returnType := .basic .tunit
-  , locals := [LocalVar.mk ⟨"x"⟩ (.basic typeT)]
-  , body := exampleFoo
-  }
 
 end LeanMove.Examples
